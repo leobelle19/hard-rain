39e586b6a01869a8e0a4a4291ec631fbc94b70f2
leobelle19 <44816394+leobelle19@users.noreply.github.com>
2018-11-06 12:12:22 -0700

    Add files via upload

---
+++ HelloWorld5001.py
@@ -0,0 +1,9 @@
+print("Hello World")
+var = 2018
+print("Today is Monday the 5th of November", var)
+print("It's sunny with a chance of meatballs\n"*3)
+# print (7+8)
+# print(7-8)
+# print(7*8)
+# print(7/8)
+# print(5**2)
